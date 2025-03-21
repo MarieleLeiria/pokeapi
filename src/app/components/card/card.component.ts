7d84f7e603999c0a5e6af77042f0ceb0c393f93f
Mariele <marieleleiria247@gmail.com>
2025-03-21 18:17:17 -0300

    failed render all pokemons

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,6 +7,7 @@ import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
@@ -14,9 +15,10 @@ import { Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   poke: any = null;
-  pokee: any = {}
+  pokee: any = []
 
   searchPoke: string = "";
+  pokemons: any[] = [];
 
 
   constructor(private srv: PokeDataService) { }
@@ -25,9 +27,7 @@ export class CardComponent implements OnInit {
     console.log(this.searchPoke)
     this.srv.getData(this.searchPoke).subscribe(
       (data) => {
-        this.pokee = data;
-        console.log(data)
-
+        this.pokee = data.results;
       },
       (error) => {
         console.error("erro ao buscar pokemon", error);
@@ -35,10 +35,21 @@ export class CardComponent implements OnInit {
       }
     )
 
+
   };
 
   ngOnInit() {
-    // this.getPoke()
+    this.pokee.forEach((pokemon: any) => {
+      this.srv.getData(pokemon.url).subscribe(details => {
+        this.pokemons.push({
+          pokeName: details.name,
+          types: details.types,
+          weight: details.weight,
+          height: details.height,
+          image: details.sprites.front_default
+        })
+      })
+    })
   }
 
   private inputValor = new BehaviorSubject<string>(''); // Estado inicial vazio
@@ -49,17 +60,16 @@ export class CardComponent implements OnInit {
   atualizarValorinput(event: Event) {
     const valor = (event.target as HTMLInputElement).value; // Fazendo type assertion
     this.inputValor.next(valor); // Atualiza o estado
-    this.searchPoke = valor
-    console.log("atualizou")
+    this.searchPoke = valor;
 
   }
 
   @Input() pokeName?: string;
   @Input() weight?: number;
   @Input() primaryType?: string;
+  @Input() secondaryType?: string;
   @Input() height?: number;
-  @Input() cardModel?: 'primary';
-
+  @Input() image?: any;
 }
 
 
